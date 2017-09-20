1af05f3053b9ef7b406da4c95e11a2894a7b3aa6
smiska <saroy.mihaly@gmail.com>
2017-09-20 15:13:47 +0200

    Update: fix chapters title

--- scripts/chapters.js
+++ scripts/chapters.js
@@ -37,7 +37,7 @@
 			url: "http://blog.edmdesigner.com/code-generator-used-in-html-email-development/"
 		},
 		{
-			title: "Code Modification Tools in HTML Email Development"
+			title: "Code Modification Tools Used in HTML Email Development"
 		},
 		{
 			title: "CSS Preprocessors and Inlineing in HTML Email Coding"
